cbe012c5cdd701b22e5a7fa0649e81dd3a010cb7
unknown <you@example.com>
2022-09-10 00:06:47 +0700

    Nop bao ss1_introduction_to_java

---
+++ src/ss1_introduction_to_java/ShowGreetings.java
@@ -0,0 +1,16 @@
+package Introduction_to_java;
+
+import java.util.Scanner;
+
+public class ShowGreetings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+
+        System.out.println("Input name :");
+        name = sc.nextLine();
+
+        System.out.println("Hello " + name + " !");
+
+    }
+}

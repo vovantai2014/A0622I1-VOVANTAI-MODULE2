195f4f0a6f9be21f2ebeda190fc2fff77b698f5d
unknown <you@example.com>
2022-09-26 15:54:17 +0700

    fix comment ss6,ss7

--- src/ss7_abstract_and_interface/bai_tap/interface_resizeable/Resizeable.java
+++ src/ss7_abstract_and_interface/bai_tap/interface_resizeable/Resizeable.java
@@ -1,5 +1,5 @@
 package ss7_abstract_and_interface.bai_tap.interface_resizeable;
 
 public interface Resizeable {
-    public abstract void resize(double percent);
+     void resize(double percent);
 }
